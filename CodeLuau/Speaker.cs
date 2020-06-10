8d88bb8c78d6257d16116a6edf667c3a04c0da6d
Simeon Olorunninwo <Simeon.olorunninwo@wemabank.com>
2020-06-10 16:41:38 +0100

    rename variables

--- CodeLuau/Speaker.cs
+++ CodeLuau/Speaker.cs
@@ -12,7 +12,7 @@ namespace CodeLuau
 		public string FirstName { get; set; }
 		public string LastName { get; set; }
 		public string Email { get; set; }
-		public int? Exp { get; set; }
+		public int? yearsExperience { get; set; }
 		public bool HasBlog { get; set; }
 		public string BlogURL { get; set; }
 		public WebBrowser Browser { get; set; }
@@ -29,12 +29,15 @@ namespace CodeLuau
 		{
 			int? speakerId = null;
 
+			var error = ValidateData();
+			if (error != null) return new RegisterResponse(error);
+
 			//put list of employers in array
-			var emps = new List<string>() { "Pluralsight", "Microsoft", "Google" };
+			var preferredEmployers = new List<string>() { "Pluralsight", "Microsoft", "Google" };
 
-			bool good = Exp > 10 || HasBlog || Certifications.Count() > 3 || emps.Contains(Employer);
+			bool speakerAppearsQualified = yearsExperience > 10 || HasBlog || Certifications.Count() > 3 || preferredEmployers.Contains(Employer);
 
-			if (!good)
+			if (!speakerAppearsQualified)
 			{
 				//need to get just the domain from the email
 				string emailDomain = Email.Split('@').Last();
@@ -42,13 +45,13 @@ namespace CodeLuau
 				
 				if (!domains.Contains(emailDomain) && (!(Browser.Name == WebBrowser.BrowserName.InternetExplorer && Browser.MajorVersion < 9)))
 				{
-					good = true;
+					speakerAppearsQualified = true;
 				}
 			}
 
-			bool appr = false;
+			bool approved = false;
 
-			if (good)
+			if (speakerAppearsQualified)
 			{
 				if (Sessions.Count() != 0)
 				{
@@ -66,7 +69,7 @@ namespace CodeLuau
 							else
 							{
 								session.Approved = true;
-								appr = true;
+								approved = true;
 							}
 						}
 					}
@@ -76,25 +79,25 @@ namespace CodeLuau
 					return new RegisterResponse(RegisterError.NoSessionsProvided);
 				}
 
-				if (appr)
+				if (approved)
 				{
 					//if we got this far, the speaker is approved
 					//let's go ahead and register him/her now.
 					//First, let's calculate the registration fee. 
 					//More experienced speakers pay a lower fee.
-					if (Exp <= 1)
+					if (yearsExperience <= 1)
 					{
 						RegistrationFee = 500;
 					}
-					else if (Exp >= 2 && Exp <= 3)
+					else if (yearsExperience >= 2 && yearsExperience <= 3)
 					{
 						RegistrationFee = 250;
 					}
-					else if (Exp >= 4 && Exp <= 5)
+					else if (yearsExperience >= 4 && yearsExperience <= 5)
 					{
 						RegistrationFee = 100;
 					}
-					else if (Exp >= 6 && Exp <= 9)
+					else if (yearsExperience >= 6 && yearsExperience <= 9)
 					{
 						RegistrationFee = 50;
 					}
@@ -130,7 +133,7 @@ namespace CodeLuau
 		}
 
 
-		private RegisterError? ValidateDate()
+		private RegisterError? ValidateData()
 		{
 			if (string.IsNullOrWhiteSpace(FirstName)) return RegisterError.FirstNameRequired;
 			
